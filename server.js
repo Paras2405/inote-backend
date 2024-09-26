6bf58431b3b42e802d6ee81ad76fa32a0f005edb
Paras2405 <159181551+Paras2405@users.noreply.github.com>
2024-09-26 19:59:27 +0530

    Backend updated

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const db=require('./db');
+const PORT=process.env.PORT||3000
+const express=require('express')
+const cors= require('cors')
+
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/auth',require('./routes/auth')
+)
+app.use('/api/notes',require('./routes/notes'))
+
+app.listen(PORT,()=>{
+    console.log(`App listening at ${PORT}`)
+})
